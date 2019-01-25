cf85032ddb8e6af0a11e34f788dd8c3b8eb8f5e1
rharish101 <harish.rajagopals@gmail.com>
2019-01-25 16:54:59 +0530

    Added regexes for all; error in comments

--- lexer.py
+++ lexer.py
@@ -1,26 +1,6 @@
 """Lexer for Go."""
 from ply import lex
 
-tokens = (
-    "COMMENTS",
-    "STRING",
-    # "RUNE",
-    "ID",
-    "OP",
-    "INT",
-    "FLOAT",
-    "IMAG",
-    "SEMICOLONS",
-)
-t_COMMENTS = r"(//.*|/\*[\s\S]*\*/)"
-t_STRING = r"\"[^\"\n]+\""
-t_RUNE = r""
-t_OP = r""
-t_INT = r""
-t_FLOAT = r""
-t_IMAG = r""
-t_SEMICOLONS = r""
-
 reserved = [
     "break",
     "default",
@@ -49,16 +29,41 @@ reserved = [
     "var",
 ]
 
+tokens = [
+    "COMMENT",
+    "STRING",
+    # "RUNE",
+    "ID",
+    "KEYWORD",
+    "OP",
+    "INT",
+    "FLOAT",
+    "IMAG",
+    "SEMICOLON",
+    "NEWLINE",
+]
+t_COMMENT = r"(//.*|/\*[\s\S]*\*/)"
+t_STRING = r"\"[^\"\n]+\""
+# t_RUNE = r""
+t_OP = r"(\+|&|\+=|&=|&&|==|!=|\(|\)|-|\||-=|\|=|\|\||<|<=|\[|\]|\*|\^|\*=|\^=|<-|>|>=|\{|\}|/|<<|/=|<<=|\+\+|=|:=|,|;|%|>>|%=|>>=|--|!|\.\.\.|\.|:|&\^|&\^=)"
+t_INT = r"([1-9][0-9]*|0[0-7]*|0[xX][0-9a-fA-F]+)"
+t_FLOAT = r"([0-9]+\.([0-9]+)?((e|E)(\+|-)?[0-9]+)?|[0-9]+(e|E)(\+|-)?[0-9]+|\.[0-9]+((e|E)(\+|-)?[0-9]+)?)"
+t_IMAG = r"([0-9]+|([0-9]+\.([0-9]+)?((e|E)(\+|-)?[0-9]+)?|[0-9]+(e|E)(\+|-)?[0-9]+|\.[0-9]+((e|E)(\+|-)?[0-9]+)?))i"
+t_SEMICOLON = r";"
+t_NEWLINE = r"\n"
+t_ignore = " \t"
+
 
 def t_ID(t):
-    r"[a-zA-Z][a-zA-Z_0-9]"
+    r"[a-zA-Z][a-zA-Z_0-9]*"
     if t.value in reserved:
-        t.type = t.value.upper()
+        t.type = "KEYWORD"
     return t
 
 
 lexer = lex.lex()
-lexer.input()
+with open("/home/rharish/Programs/Go/hello.go", "r") as go:
+    lexer.input(go.read())
 while True:
     token = lexer.token()
     if not token:

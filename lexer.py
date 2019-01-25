aae74417b64ee077e60893cd761a59eb6aec097e
rharish101 <harish.rajagopals@gmail.com>
2019-01-25 17:17:47 +0530

    Made it readable and executable

--- lexer.py
+++ lexer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """Lexer for Go."""
 from ply import lex
 
@@ -39,17 +40,76 @@ tokens = [
     "INT",
     "FLOAT",
     "IMAG",
-    "SEMICOLON",
     "NEWLINE",
 ]
 t_COMMENT = r"(//.*|/\*[\s\S]*\*/)"
 t_STRING = r"\"[^\"\n]+\""
 # t_RUNE = r""
-t_OP = r"(\+|&|\+=|&=|&&|==|!=|\(|\)|-|\||-=|\|=|\|\||<|<=|\[|\]|\*|\^|\*=|\^=|<-|>|>=|\{|\}|/|<<|/=|<<=|\+\+|=|:=|,|;|%|>>|%=|>>=|--|!|\.\.\.|\.|:|&\^|&\^=)"
-t_INT = r"([1-9][0-9]*|0[0-7]*|0[xX][0-9a-fA-F]+)"
-t_FLOAT = r"([0-9]+\.([0-9]+)?((e|E)(\+|-)?[0-9]+)?|[0-9]+(e|E)(\+|-)?[0-9]+|\.[0-9]+((e|E)(\+|-)?[0-9]+)?)"
-t_IMAG = r"([0-9]+|([0-9]+\.([0-9]+)?((e|E)(\+|-)?[0-9]+)?|[0-9]+(e|E)(\+|-)?[0-9]+|\.[0-9]+((e|E)(\+|-)?[0-9]+)?))i"
-t_SEMICOLON = r";"
+
+operators = [
+    r"\+",
+    r"&",
+    r"\+=",
+    r"&=",
+    r"&&",
+    r"==",
+    r"!=",
+    r"\(",
+    r"\)",
+    r"-",
+    r"\|",
+    r"-=",
+    r"\|=",
+    r"\|\|",
+    r"<",
+    r"<=",
+    r"\[",
+    r"\]",
+    r"\*",
+    r"\^",
+    r"\*=",
+    r"\^=",
+    r"<-",
+    r">",
+    r">=",
+    r"\{",
+    r"\}",
+    r"/",
+    r"<<",
+    r"/=",
+    r"<<=",
+    r"\+\+",
+    r"=",
+    r":=",
+    r",",
+    r";",
+    r"%",
+    r">>",
+    r"%=",
+    r">>=",
+    r"--",
+    r"!",
+    r"\.\.\.",
+    r"\.",
+    r":",
+    r"&\^",
+    r"&\^=",
+]
+t_OP = r"(" + r"|".join(operators) + r")"
+
+decimal_lit = r"[1-9][0-9]*"
+octal_lit = r"0[0-7]*"
+hex_lit = r"0[xX][0-9a-fA-F]+"
+t_INT = r"(" + decimal_lit + "|" + octal_lit + "|" + hex_lit + ")"
+
+decimals = r"[0-9]+"
+exponent = r"(e|E)[\+-]?" + decimals
+float_lit_1 = decimals + r"\." + r"(" + decimals + r")?(" + exponent + r")?"
+float_lit_2 = decimals + exponent
+float_lit_3 = r"\." + decimals + r"(" + exponent + r")?"
+t_FLOAT = r"(" + float_lit_1 + r"|" + float_lit_2 + r"|" + float_lit_3 + r")"
+
+t_IMAG = r"(" + decimals + r"|" + t_FLOAT + ")i"
 t_NEWLINE = r"\n"
 t_ignore = " \t"
 

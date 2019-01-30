1b6814fb99dd85974d9b09c6c7237583263ebd71
rharish101 <harish.rajagopals@gmail.com>
2019-01-30 11:34:34 +0530

    Fixed string regex for including escaped double-quotes; updated testcase

--- assign_1/tests/input1/hello.go
+++ assign_1/tests/input1/hello.go
@@ -7,7 +7,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(`hello /*world*/`)
+	fmt.Println("help \"me\"")
+	fmt.Println(`hello /*world*/
+	Yo buddy
+	`)
 	fmt.Println('\n')
 	fmt.Println(.2e0 / (2 + 3i))
 	fmt.Println(0x5 &^ 0x1)

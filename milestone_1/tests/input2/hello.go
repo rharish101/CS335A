c97e1f4aa016a88b95755096befd2374338155cb
rharish101 <harish.rajagopals@gmail.com>
2019-02-14 22:38:35 +0530

    Added some testcases

---
+++ milestone_1/tests/input2/hello.go
@@ -0,0 +1,19 @@
+package main
+
+// Yo
+/* Hello "world" program
+   In Go*/
+
+import "fmt"
+
+func main() {
+	a := [2]int{1, 2}
+	fmt.Println(a)
+	fmt.Println("help \"me\"")
+	fmt.Println(`hello /*world*/
+	Yo buddy
+	`)
+	fmt.Println('\n')
+	fmt.Println(.2e0 / (2 + 3i))
+	fmt.Println(0x5 &^ 0x1)
+}

4885a5d6578a4f2644392effd7c7e7e72720725c
VishwasLathi <lathivishwas@gmail.com>
2019-03-10 20:30:51 +0530

    added testcase

---
+++ milestone_2/tests/hello.go
@@ -0,0 +1,48 @@
+package main
+
+// Yo
+/* Hello "world" program
+   In Go*/
+
+var c float  =  2.5
+
+type person struct {
+    name string
+    age  int
+}
+
+type T struct {
+    f1     string "f one"
+    f2     string
+    f3     string `f `
+    f4, f5 int64  `f four and five`
+}
+
+func temp(x int,y int ){
+	d := 4
+	var b int = 3
+	c := d + b
+	a  := c
+	// var c int = 20
+	// // d = 3
+	s:= "string"
+	e := d + 4*5 + c
+	a = e
+	{
+
+		z := 4
+		s = "string1"
+	}
+}
+
+func main() {
+	
+	// s := "string"
+	// var a int = 4
+	// b := a + 4*5 
+	// a:=3+4
+	// b :=a*5
+	// c:=a-b
+
+
+}

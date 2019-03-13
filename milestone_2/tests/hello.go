c1d490d7879d6cf9f6636fa17f6334ba5190717b
VishwasLathi <lathivishwas@gmail.com>
2019-03-13 16:54:56 +0530

    added testcases

--- milestone_2/tests/hello.go
+++ milestone_2/tests/hello.go
@@ -4,7 +4,7 @@ package main
 /* Hello "world" program
    In Go*/
 
-// var c int64  =  2
+var c int64  =  2
 
 
 type person struct {
@@ -14,47 +14,47 @@ type person struct {
 type custom person
 type custom1 custom
 
-// type T struct {
-//     f1     string "f one"
-//     f2     string
-//     f3     string `f `
-//     f4, f5 int64  `f four and five`
-// }
+type T struct {
+    f1     string "f one"
+    f2     string
+    f3     string `f `
+    f4, f5 int64  `f four and five`
+}
 
-// func temp1(x uint, y uint)uint{
-// 	return x
-// }
+func temp1(x uint, y uint)uint{
+	return x
+}
 
-// func add(x uint, y int)uint {
-// 	return x+y
-// }
+func add(x uint, y int)uint {
+	return x+y
+}
 
 func temp(x int,y int ){
-	r := custom1{"vishwas",2}
+	r := custom{"vishwas",2}
 	f := r.age
-	// m := 2
-	// n := &m
-	// t := person{"string",2}
-	// t.age = 2
-	// k := ++t.age + ++(*n)
+	m := 2
+	n := &m
+	t := person{"string",2}
+	t.age = 2
+	k := ++t.age + ++(*n)
  	
-	// var d uint= 4
-	// var b uint = 3
-	// c := d + b
-	// a  := c
-	// var z uint = 20
-	// d = 3
-	// primes := [6]uint{2, 3, 5, 7, 11, 13}
-	// s:= "string"
-	// // d = temp1(2,3)
-	// e := 4*5 + c + primes[2] + add(temp1(2,3),3)
-	// primes1 := [2]int{2,3,4}
-	// primes[primes1[2]] = z
-	// var val uint = add(2,3)
+	var d uint= 4
+	var b uint = 3
+	c := d + b
+	a  := c
+	var z uint = 20
+	d = 3
+	primes := [6]uint{2, 3, 5, 7, 11, 13}
+	s:= "string"
+	// d = temp1(2,3)
+	e := 4*5 + c + primes[2] + add(temp1(2,3),3)
+	primes1 := [2]int{2,3,4}
+	primes[primes1[2]] = z
+	var val uint = add(2,3)
 
 
 
-	// primes[e] = 23
+	primes[e] = 23
 
 	// var temp int  = person{name: "Alice", age: 30}
 	// s1 := person{name: "Sean", age: 50}

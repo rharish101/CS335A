46f476c5c3adb33968642ed71a7ded038106319a
VishwasLathi <lathivishwas@gmail.com>
2019-04-03 22:05:36 +0530

    modified testcase

--- milestone_3/tests/hello.go
+++ milestone_3/tests/hello.go
@@ -54,10 +54,10 @@ func temp(x int, y int) {
 	add := oneD[1] + twoD[2][1]
 	gu := help{"vishwas"}
 	r := person{gu, 2}
-	struct_array := [2]help{gu,gu}
-	name := struct_array[1]
+	struct_array := [1][2]help{{gu,gu}}
+	name := struct_array[1][2]
 	item := name.b
-	name1 := struct_array[1].b
+/*	name1 := struct_array[1].b*/
 
 	f := [3]int{1, 2, 3}
 	var g [3]int

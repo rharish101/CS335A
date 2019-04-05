67a7c48acb4f9e0bc156ec45576bd93b8e8fe8c8
VishwasLathi <lathivishwas@gmail.com>
2019-04-05 21:27:21 +0530

    added functions.go

---
+++ milestone_3/tests/functions.go
@@ -0,0 +1,37 @@
+//shows functions, their activation records and semantic checking on operations using their return types 
+
+package main
+type int_t uint
+
+func temp1(x uint, y uint) uint {
+	return x
+}
+
+func temp2(x uint, y uint) (uint, uint) {
+	return x, y
+}
+
+func add(x uint, y uint) uint {
+	return x + y
+}
+
+func temp(x int,y int){
+	var d int_t = 4
+	var b uint = 3
+	b, b = temp2(b, b)
+	c := uint(d) + b
+
+	a := c
+	var z uint = 20
+	d = 3
+	primes := [3]uint{2, 3, 4}
+
+	s := "string"
+
+	e := 4*5 + c + primes[2] + add(temp1(2, 3), 3)
+	primes1 := [3]int{2, 3, 4}
+	primes[primes1[2]] = z
+	var val uint = add(2, 3)
+
+	primes[e] = 23
+}

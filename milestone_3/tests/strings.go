6d6a448cecba9bcb6b302f73e9b5c5bc3297c0fe
VishwasLathi <lathivishwas@gmail.com>
2019-04-05 21:41:32 +0530

    added multiple testcases

---
+++ milestone_3/tests/strings.go
@@ -0,0 +1,16 @@
+package main
+
+func main(){
+	oneD := [2]string{"name", "age"}
+	oneD[1] = "vishwas"
+	
+		emp3 := struct {
+		firstName, lastName string
+		age, salary         int
+	}{
+		firstName: "Andreah",
+		lastName:  "Nikola",
+		age:       2,
+		salary:    5000,
+	}
+}

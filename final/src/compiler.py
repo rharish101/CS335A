7c8a30f69985b18676ec4c7301d25f7afc9ea100
rharish101 <harish.rajagopals@gmail.com>
2019-04-09 11:50:02 +0530

    Initial commit for translating 3AC to MIPS
    Global scope commands now bunched up in main.
    Added label-based syntax for functions.
    Global variables stored in data segment.

--- final/src/compiler.py
+++ final/src/compiler.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Compiler for Go to MIPS."""
+from go_classes import *
 from intermediate import process_code
 from argparse import ArgumentParser
 import logging
@@ -16,7 +17,52 @@ def ir2mips(table, ir_code):
         str: The MIPS assembly code for the given 3AC
 
     """
-    return ir_code
+    # Store global variables in the data segment
+    mips = ".data:\n"
+    for collection in [table.variables, table.intermediates]:
+        for var in collection:
+            mips += " " * 4 + ".align 2\n"
+            mips += " " * 4 + var + ": "
+            dtype = collection[var]
+            size = table.get_size(dtype)
+            mips += ".space {}\n".format(size)
+    mips += "\n"
+
+    mips += ".text\n.globl main\n\n"
+
+    # Get all lines in the global scope
+    global_lines = []
+    global_line_indices = set()
+    outside_func = True
+    for i, line in enumerate(ir_code.strip().split("\n")):
+        if line.startswith("func begin"):
+            outside_func = False
+        if outside_func:
+            global_lines.append(line)
+            global_line_indices.add(i)
+        if line.startswith("func end"):
+            outside_func = True
+
+    indent = 0
+    for i, line in enumerate(ir_code.strip().split("\n")):
+        if i in global_line_indices:
+            continue
+
+        if line.startswith("func begin"):
+            mips += " " * indent + "{}:\n".format(line.split()[-1])
+            indent += 4
+            # Move all global declarations into the main function
+            if line.startswith("func begin main"):
+                for item in global_lines:
+                    mips += " " * indent + item + "\n"
+
+        elif line == "func end":
+            mips += "\n"
+            indent -= 4
+
+        else:
+            mips += " " * indent + line + "\n"
+    return mips.strip()
 
 
 if __name__ == "__main__":

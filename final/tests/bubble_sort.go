fecb0da8054a09e5e2872c368f70aae431c35a75
VishwasLathi <lathivishwas@gmail.com>
2019-04-15 23:06:29 +0530

    modified testcases

--- final/tests/bubble_sort.go
+++ final/tests/bubble_sort.go
@@ -1,19 +1,31 @@
 package main
 
-func BubbleSort(items [5]int)  {
-
+func BubbleSort(source *[5]int)  {
    L := 5
+   items := *source
+   //items := [5]int{4,2,1,5,6}
+   // for(i:=0;i<5;i++){
+   //    println(items[i])
+   // }
    for  (i:=0;i<L;i++){
       for (j:=0;j<(L-1-i);j++){
          if (items[j] > items[j+1]){
-            items[j], items[j+1] = items[j+1], items[j]
+            temp := items[j]
+            items[j] = items[j+1]
+            items[j+1] = temp
          }
       }
+   }
+   for(i:=0;i<5;i++){
+      println(items[i])
    }
 
 }
 
 func main(){
 	a := [5]int{4,2,1,5,6}
-	BubbleSort(a)
+	BubbleSort(&a)
+   // for(i:=0;i<5;i++){
+   //    println(a[i])
+   // }
 }
--- final/tests/typedef.go
+++ final/tests/typedef.go
@@ -1,17 +1,18 @@
 package main
 
 type info1 struct {
-	info string
+	info int
 }
 
 type person struct {
 	info1
-	name string
+	name int
 	age  int
 }
 type custom person
 type custom1 custom
 
 func main(){
-	a := custom1{info1{"vishwas"},"name",2}
+	a := custom1{info1{2},3,4}
+	println(a.name,a.info1.info,a.age)
 }

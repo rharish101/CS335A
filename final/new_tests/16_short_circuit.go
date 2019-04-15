b428ae5bf0d30e3571b04bd0276a74405bf87ebc
siddharthnohria <siddharth.nohria@gmail.com>
2019-04-16 01:47:23 +0530

    Modified testcase and bugs.txt

--- final/new_tests/16_short_circuit.go
+++ final/new_tests/16_short_circuit.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt"
 
-func callMyFunc() int {
+func callMyFunc() bool {
 	//println("This should not print\n")
 	println(0)
-	return 1
+	return true
 }
 
 func main() {
 	a := 0
-	if (a >= 0 || callMyFunc()>0) {
+	if (a >= 0 || callMyFunc()) {
 		//println("This should print in first line\n")
 		println(1)
 	}
-	if (a > 0 && callMyFunc()>0) {
+	if (a > 0 && callMyFunc()) {
 		//println("This should not print in second line\n")
 		println(2)
 	} else {

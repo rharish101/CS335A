4db7f5f77bd714343ef7e2a8ffec5ddc52b91b0f
rharish101 <harish.rajagopals@gmail.com>
2019-04-16 23:47:54 +0530

    Reverted previous hotfix

--- final/src/intermediate.py
+++ final/src/intermediate.py
@@ -2439,7 +2439,12 @@ def symbol_table(
                     DTYPE = lhs_dtype.dtype
 
                 ir_code += lhs_code
-                table.insert_var(lhs_name, lhs_dtype, use="intermediate")
+                if isinstance(lhs_dtype, GoArray):
+                    table.insert_var(
+                        lhs_name,
+                        GoPointType(GoType("int")),
+                        use="intermediate",
+                    )
 
                 rhs_name = "__indrhs_{}".format(inter_count)
                 rhs_dtype, rhs_code = symbol_table(
